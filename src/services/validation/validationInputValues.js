f7abf0cb8624c41163c5d1782d0f531abaf9c87b
GusCabraal <gustavocabral97@hotmail.com>
2022-09-11 10:56:34 -0300

    fix: validation of error in new product register

--- src/services/validation/validationInputValues.js
+++ src/services/validation/validationInputValues.js
@@ -2,10 +2,13 @@ const { addNewProduct } = require('./schemas');
 
 const validateNewProductSchema = (name) => {
   const { error } = addNewProduct.validate({ name });
-  if ((error.message === '"name" is required')) {
-    return { type: 'BAD_REQUEST', message: error.message };
-  } if (error.message) {
-    return { type: 'INVALID_VALUE', message: error.message };
+  if (error) {
+    if (error.message === '"name" is required') {
+      return { type: 'BAD_REQUEST', message: error.message };
+    }
+    if (error.message) {
+      return { type: 'INVALID_VALUE', message: error.message };
+    }
   }
   return { type: null, message: '' };
 };
